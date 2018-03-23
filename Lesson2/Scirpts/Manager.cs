c47c318f19411d4501156c5ab9cf7fccbf7409be
keven2148 <33781038+keven2148@users.noreply.github.com>
2018-03-23 16:59:24 +0800

    Add files via upload

---
+++ Lesson2/Scirpts/Manager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour {
+
+    public GameObject borad;
+    public GameObject message;
+    private GameObject _board;
+    private GameObject _message;
+
+    void Start() {
+        _board = Instantiate(borad);
+        _message = Instantiate(message);
+    }
+
+    public void OnSelectOnZone(int x, int y, int symbol) 
+    {
+        if (symbol == 1) _message.GetComponent<Message>().OnChangeBox(x, y, "√");
+        else _message.GetComponent<Message>().OnChangeBox(x, y, "○");
+    }
+
+    public void OnWin(string name)
+    {
+        _message.GetComponent<Message>().OnChangeWinnerMessage(name + " Win this game!!!!");
+    }
+
+    public void OnReStart() {
+        Destroy(_board);
+        Destroy(_message);
+        _board = Instantiate(borad);
+        _message = Instantiate(message);
+    }
+}
